6189df6b10c436643ca78174f06ae1e53f8c212b
Sheratan095 <marco.ceccarelli.code@gmail.com>
2026-02-22 19:08:15 +0100

    basic wave

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 
 
 void DrawHUD(Screen& scr, Game& game, long startTime, int playerCount, int endless);
-
+void storyMode(Game& game, int score);
 void AddEnemies(Game& game, int score);
 
 void	switchInput(int ch, Game *game, Screen& scr);
@@ -15,48 +15,45 @@ int main() {
 	Screen scr;
 	scr.init();
 	int marginPlayersLine = 5;
-    int endless = 1; // Default to endless mode
-    // Show start menu to choose number of players
-    int players = ShowMenu(scr, &endless);
+	int endless = 1; // Default to endless mode
+	// Show start menu to choose number of players
+	int players = ShowMenu(scr, &endless);
 
-    int h = scr.getHeight();
+	int h = scr.getHeight();
 	int w = scr.getWidth();
 
-    Game game(players, h - marginPlayersLine, w, h); // Start with selected player count
+	Game game(players, h - marginPlayersLine, w, h); // Start with selected player count
 
-    game.start();
-    game.addMinion(); // Start with one enemy
-    game.addAsteroid(); // Start with one asteroid
-    game.addBoss(); // Start with one boss
+	game.start();
 	while (game.isRunning())
 	{
 		// Get current dimvoid AddEnemies(Game& game, int score, int screenWidth, int screenHeight)ensions
-        w = scr.getWidth();
-        h = scr.getHeight();
+		w = scr.getWidth();
+		h = scr.getHeight();
 
-        if (frame_count % (CLOCKS_PER_SEC * 10) == 0)
-        {
-            if (endless == 1) // Only add enemies in endless mode
-                AddEnemies(game, game.getScore());
-            //else
-                // Story mode: add a boss every 500 points
-        } // Add new enemies every 10 seconds in endless mode 
-        // Update every 60th of a second
-        if (frame_count % (CLOCKS_PER_SEC / 6) == 0)
-        {
+		if (frame_count % (CLOCKS_PER_SEC * 10) == 0)
+		{
+			if (endless == 1) // Only add enemies in endless mode
+				AddEnemies(game, game.getScore());
+			else
+				storyMode(game, game.getScore());
+		} // Add new enemies every 10 seconds in endless mode 
+		// Update every 60th of a second
+		if (frame_count % (CLOCKS_PER_SEC / 6) == 0)
+		{
 			scr.clear(); // DONT PUT FUNCTIONS BACK 
-            DrawHUD(scr, game, scr.getCurrentTime() / CLOCKS_PER_SEC, players, endless);
-            game.showEntities();
+			DrawHUD(scr, game, scr.getCurrentTime() / CLOCKS_PER_SEC, players, endless);
+			game.showEntities();
 			scr.refresh();
-        }
-        if (frame_count % (CLOCKS_PER_SEC * 5) == 0)
-            game.addAsteroid(); // Add new asteroids every 5 seconds
-        if (frame_count % (CLOCKS_PER_SEC * 3) == 0) // Handle input every 10th of a second
+		}
+		if (frame_count % (CLOCKS_PER_SEC * 5) == 0)
+			game.addAsteroid(); // Add new asteroids every 5 seconds
+		if (frame_count % (CLOCKS_PER_SEC * 3) == 0) // Handle input every 10th of a second
 			game.addStar(); // Add new stars to the background every 3rd of a second
-        if (frame_count % (CLOCKS_PER_SEC / 10) == 0)
-            game.update(); // Update game state every 10th of a second
+		if (frame_count % (CLOCKS_PER_SEC / 10) == 0)
+			game.update(); // Update game state every 10th of a second
 
-        frame_count++;
+		frame_count++;
 		switchInput(getch(), &game, scr);
 	}
 	return 0;
@@ -64,114 +61,136 @@ int main() {
 
 void DrawHUD(Screen& scr, Game& game, long startTime, int playerCount, int endless)
 {
-    int h = scr.getHeight();
-    int w = scr.getWidth();
+	int h = scr.getHeight();
+	int w = scr.getWidth();
 
-    // background color for HUD area
-    wattron(stdscr, COLOR_PAIR(UHD_COLOR_PAIR));
+	// background color for HUD area
+	wattron(stdscr, COLOR_PAIR(UHD_COLOR_PAIR));
 
-    box(stdscr, 0, 0); // draw a box of height 5 at the bottom of the screen
-    // separator line above HUD
-    mvhline(h - 5, 0, ' ', w); // clear the row just above HUD
-    mvhline(h - 4, 0, '-', w);
+	box(stdscr, 0, 0); // draw a box of height 5 at the bottom of the screen
+	// separator line above HUD
+	mvhline(h - 5, 0, ' ', w); // clear the row just above HUD
+	mvhline(h - 4, 0, '-', w);
 
-    // clear HUD rows before drawing (in case text from previous frame lingers)
-    for (int row = h - 3; row < h; ++row)
-        mvhline(row, 0, ' ', w);
+	// clear HUD rows before drawing (in case text from previous frame lingers)
+	for (int row = h - 3; row < h; ++row)
+		mvhline(row, 0, ' ', w);
 
-    // primary stats line
-    int col = 2;
-    mvprintw(h - 3, col, "Score: %d", game.getScore());
-    col += 15;
+	// primary stats line
+	int col = 2;
+	mvprintw(h - 3, col, "Score: %d", game.getScore());
+	col += 15;
 
-    if (playerCount > 1)
-    {
-        mvprintw(h - 3, col, "P1 HP: %d", game.getPlayerHealth(0));
-        col += 15;
-        mvprintw(h - 3, col, "P2 HP: %d", game.getPlayerHealth(1));
-    }
-    else
-    {
-        mvprintw(h - 3, col, "HP: %d", game.getPlayerHealth(0));
-    }
+	if (playerCount > 1)
+	{
+		mvprintw(h - 3, col, "P1 HP: %d", game.getPlayerHealth(0));
+		col += 15;
+		mvprintw(h - 3, col, "P2 HP: %d", game.getPlayerHealth(1));
+	}
+	else
+	{
+		mvprintw(h - 3, col, "HP: %d", game.getPlayerHealth(0));
+	}
 
-    // time on right side
-    mvprintw(h - 3, w - 20, "Time: %lds", startTime);
-    mvprintw(h - 2, w - 20, "MODE: %s", (endless == 1 ? "Endless" : "Story"));
+	// time on right side
+	mvprintw(h - 3, w - 20, "Time: %lds", startTime);
+	mvprintw(h - 2, w - 20, "MODE: %s", (endless == 1 ? "Endless" : "Story"));
 
-    // controls row
-    mvprintw(h - 2, 2, "Controls: A/D Move  W Shoot  Q Quit");
+	// controls row
+	mvprintw(h - 2, 2, "Controls: A/D Move  W Shoot  Q Quit");
 
-    wattr_off(stdscr, COLOR_PAIR(UHD_COLOR_PAIR), 0);
+	wattr_off(stdscr, COLOR_PAIR(UHD_COLOR_PAIR), 0);
 }
 
 void AddEnemies(Game& game, int score)
 {
-    static int lastBossScore = 0;
+	static int lastBossScore = 0;
 
-    int level = score / 100; 
+	int level = score / 100; 
 
-    int spawnChance = 30 + level * 10; 
+	int spawnChance = 30 + level * 10; 
 
-    if (rand() % 100 < spawnChance)
-    {
-        int roll = rand() % 100;
+	if (rand() % 100 < spawnChance)
+	{
+		int roll = rand() % 100;
 
-        if (roll < 20)
-            game.addAsteroid();
-        else
-            game.addMinion();
-    }
+		if (roll < 20)
+			game.addAsteroid();
+		else
+			game.addMinion();
+	}
 
-    if (score >= lastBossScore + 1000)
-    {
-        game.addBoss();
-        lastBossScore = score;
-    }
+	if (score >= lastBossScore + 1000)
+	{
+		game.addBoss();
+		lastBossScore = score;
+	}
 }
+
 void switchInput(int ch, Game *game, Screen& scr)
 {
 
-    switch (ch)
-    {
-        // Player 0
-        case 'a':
-        case 'A':
-            game->playerMove(0, -1);
-            break;
+	switch (ch)
+	{
+		// Player 0
+		case 'a':
+		case 'A':
+			game->playerMove(0, -1);
+			break;
 
-        case 'd':
-        case 'D':
-            game->playerMove(0, 1);
-            break;
+		case 'd':
+		case 'D':
+			game->playerMove(0, 1);
+			break;
 
-        case 'w':
-        case 'W':
-            game->playerShoot(0);
-            break;
+		case 'w':
+		case 'W':
+			game->playerShoot(0);
+			break;
 
-        // Player 1
-        case KEY_LEFT:
-            game->playerMove(1, -1);
-            break;
+		// Player 1
+		case KEY_LEFT:
+			game->playerMove(1, -1);
+			break;
 
-        case KEY_RIGHT:
-            game->playerMove(1, 1);
-            break;
-        case KEY_UP:
-            game->playerShoot(1);
-            break;
-        case KEY_RESIZE:
-            scr.handleResize();
-            break;
-        // Quit
-        case 'q':
-        case 'Q':
-            game->stop(); // Stop the game
-            break;
+		case KEY_RIGHT:
+			game->playerMove(1, 1);
+			break;
+		case KEY_UP:
+			game->playerShoot(1);
+			break;
+		case KEY_RESIZE:
+			scr.handleResize();
+			break;
+		// Quit
+		case 'q':
+		case 'Q':
+			game->stop(); // Stop the game
+			break;
 
-        default:
-            break;
-    }
+		default:
+			break;
+	}
 }
 
+void storyMode(Game& game, int score)
+{
+	int enemiesToSpawn = 3;
+
+	// Quando non ci sono nemici => nuova wave
+	if (game.getEnemyCount() == 0)
+	{
+		enemiesToSpawn = 3; // nuova wave
+		for (int i = 0; i < enemiesToSpawn; ++i)
+			game.addMinion();
+
+	}
+
+	// if (enemiesToSpawn > 0)
+	// {
+	// 	game.addMinion();
+	// 	enemiesToSpawn--;
+	// }
+
+	
+}
